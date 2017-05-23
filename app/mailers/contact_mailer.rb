1d2fd85fb0a36932e18d509b4c8cf01ec1e07140
Jerik Mitchell <jerik.mitchell@gmail.com>
2017-05-23 01:54:58 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer :: Base
+    default to: 'jerik.mitchell@gmail.com'
+    
+    def method_name
+        contact_email(name,email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+    end
+end
