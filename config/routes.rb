cb39a720cc9aeb6d19a7477b8784e04cb791ac72
Jerik Mitchell <jerik.mitchell@gmail.com>
2017-05-24 16:04:07 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  resources :users do
+    resource :profile
+  end
+  
 end
