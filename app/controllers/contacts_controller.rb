4f833afe4f2d2aa848bcbf8b6ed803df8b15f5bb
Jerik Mitchell <jerik.mitchell@gmail.com>
2017-05-24 02:57:49 +0000

    updated contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,34 @@
 class ContactsController < ApplicationController
+  
     def new
-        @contact = Contact.new
+    @contact = Contact.new
     end
+  
+ 
+  def create
     
-    def create
-        @contact = Contact.new(contact_params)
-        if @contact.save
-            name = params[:contact][:name]
-            email = params[:contact][:email]
-            body = params[:contact][:comments]
-            Contact_Mailer.contact_email(name,email,body).deliver
-            flash[:success] = "Messege Sent."
-            redirect_to new_contact_path
-        else
-            flash[:danger] = @contact.errors.full_messages.join(", ")
-            redirect_to new_contact_path
-        end
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+     
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      
+      ContactMailer.contact_email(name, email, body).deliver
+      
+      flash[:success] = "Message sent."
+      redirect_to new_contact_path
+    else
+      
+      flash[:danger] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
     end
+  end
+  
+  private
     
-    private
-        def contact_params
-            params.require(:contact).permit(:name, :email, :comments)
-        end
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
